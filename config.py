ef7926a93bf9049a2d9af93336b97ec9de8712e9
treomayao15-prog <treomayao15@gmail.com>
2026-07-08 04:02:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     
     # Telegram Bot
     BOT_TOKEN = "ĐIỀN_TOKEN_BOT_CỦA_BẠN_VÀO_ĐÂY"
-    WEB_URL = "https://ten_mien_cua_ban.com" # Thay bằng URL hosting của bạn
+    WEB_URL = "https://shop-web-xxxx.onrender.com" # Thay bằng URL hosting của bạn
     
     # Đường dẫn hệ thống
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
